1c866d229452c2f6ce649d9198a7328eb96dbe65
QuyenNV-3106 <band3106@gmail.com>
2021-08-31 17:35:20 +0700

    Edit lan 2

--- src/git/demo/HelloWorld.java
+++ src/git/demo/HelloWorld.java
@@ -12,5 +12,6 @@ package git.demo;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!!!");
+        System.out.println("hihi");
     }
 }
